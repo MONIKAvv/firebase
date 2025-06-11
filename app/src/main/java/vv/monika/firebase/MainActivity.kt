0e2933e11a0619510dd6931fc57c6f16892f6820
MONIKAvv <monikalonewolf35@gmail.com>
2025-06-11 20:52:45 +0530

    firebase setup

---
+++ app/src/main/java/vv/monika/firebase/MainActivity.kt
@@ -0,0 +1,24 @@
+package vv.monika.firebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import vv.monika.firebase.ui.theme.FirebaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
+
+
